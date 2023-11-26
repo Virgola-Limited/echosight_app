c2b625b71496531313b505ebaa9c5a32f3b25056
Chris Toynbee <ctoynbee@gmail.com>
2023-11-27 09:34:31 +1300

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/views/**/*.{html,html.erb}",
+    // "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{js}", // include these if you're using these file types in your project
+    // You can also add other paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
