2edf23173272cd43e8322b9e8efe48fce8c8e879
Chris Toynbee <ctoynbee@gmail.com>
2023-11-28 12:01:41 +1300

    Tidy up form

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -23,9 +23,25 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
     super(method, class: default_input_classes(options))
   end
 
-  # ... any other field types you want to customize ...
+  def submit(value=nil, options={})
+    options[:class] ||= submit_default_classes
+    super(value, options)
+  end
+
+  def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
+    options[:class] ||= checkbox_default_classes
+    super(method, options, checked_value, unchecked_value)
+  end
 
   private
+
+  def checkbox_default_classes
+    'w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600'
+  end
+
+  def submit_default_classes
+    'w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+  end
 
   def default_form_classes(existing_classes)
     default_classes = 'mt-8 space-y-6'
