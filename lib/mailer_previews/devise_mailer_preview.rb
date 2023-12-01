46448f818e24fefe43a02633766e2988d420f6a2
Chris Toynbee <ctoynbee@gmail.com>
2023-12-01 16:03:48 +1300

    Update email content for authentication emails

--- lib/mailer_previews/devise_mailer_preview.rb
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -1,8 +1,30 @@
 # test/mailers/previews/devise_mailer_preview.rb
 
 class DeviseMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/devise_mailer/confirmation_instructions
+  # Preview the confirmation instructions email
   def confirmation_instructions
-   User.first.send_confirmation_instructions
+    User.first.send_confirmation_instructions
+  end
+
+  # Preview the email changed notification email
+  def email_changed
+    user = User.first
+    user.unconfirmed_email = "new_email@example.com" # Mock new email
+    Devise::Mailer.email_changed(user)
+  end
+
+  # Preview the password change confirmation email
+  def password_change
+    Devise::Mailer.password_change(User.first)
+  end
+
+  # Preview the reset password instructions email
+  def reset_password_instructions
+    User.first.send_reset_password_instructions
+  end
+
+  # Preview the unlock instructions email
+  def unlock_instructions
+    User.first.send_unlock_instructions
   end
 end
