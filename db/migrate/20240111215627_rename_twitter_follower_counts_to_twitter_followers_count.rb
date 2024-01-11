302c0c2869a000720d982e1480af6974c1478f93
Chris Toynbee <ctoynbee@gmail.com>
2024-01-12 11:53:45 +1300

    Fix bug

--- db/migrate/20240111215627_rename_twitter_follower_counts_to_twitter_followers_count.rb
+++
@@ -1,5 +0,0 @@
-class RenameTwitterFollowerCountsToTwitterFollowersCount < ActiveRecord::Migration[7.1]
-  def change
-    rename_table :twitter_follower_counts, :twitter_followers_count
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_215627) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_11_220053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,27 +40,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_215627) do
     t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
-  create_table "twitter_followers", force: :cascade do |t|
-    t.bigint "twitter_user_id", null: false
-    t.string "follower_id", null: false
-    t.string "follower_username"
-    t.string "follower_name"
-    t.datetime "followed_at"
-    t.bigint "identity_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["follower_id"], name: "index_twitter_followers_on_follower_id"
-    t.index ["identity_id"], name: "index_twitter_followers_on_identity_id"
-    t.index ["twitter_user_id"], name: "index_twitter_followers_on_twitter_user_id"
-  end
-
-  create_table "twitter_followers_count", force: :cascade do |t|
+  create_table "twitter_followers_counts", force: :cascade do |t|
     t.string "followers_count"
     t.bigint "identity_id", null: false
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["identity_id"], name: "index_twitter_followers_count_on_identity_id"
+    t.index ["identity_id"], name: "index_twitter_followers_counts_on_identity_id"
+  end
+
+  create_table "twitter_likes_counts", force: :cascade do |t|
+    t.string "likes_count"
+    t.bigint "identity_id", null: false
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["identity_id"], name: "index_twitter_likes_counts_on_identity_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -93,6 +88,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_215627) do
 
   add_foreign_key "hourly_tweet_counts", "identities"
   add_foreign_key "identities", "users"
-  add_foreign_key "twitter_followers", "identities"
-  add_foreign_key "twitter_followers_count", "identities"
+  add_foreign_key "twitter_followers_counts", "identities"
+  add_foreign_key "twitter_likes_counts", "identities"
 end
