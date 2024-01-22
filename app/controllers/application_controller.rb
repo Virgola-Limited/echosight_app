dd67aa9037507386f67b06b44887423b3fede8af
Chris Toynbee <ctoynbee@gmail.com>
2024-01-22 15:08:46 +1300

    Add basic auth on staging Add default banner image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_or_guest_user
+  before_action :authenticate_staging
+
+  private
+
+  def authenticate_staging
+    return unless Rails.env.staging?
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['SITE_USERNAME'] && password == ENV['SITE_PASSWORD']
+    end
+  end
 
   def current_or_guest_user
     if user_signed_in?
