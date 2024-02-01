c666e3920709de37b289c11cb54d35dff98f9276
Chris Toynbee <ctoynbee@gmail.com>
2024-02-02 08:03:39 +1300

    Add dashboard to show failed twitter fetches

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,18 @@
-# frozen_string_literal: true
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    section "Incomplete User Twitter Data Updates" do
+      table_for UserTwitterDataUpdate.joins(identity: :user).where(completed_at: nil).order('started_at DESC').limit(10) do
+        column :started_at
+        column "Error Message", :error_message
+        column "Identity UID", :identity_id do |update|
+          update.identity.uid # Assuming `uid` is a column in your `identities` table
+        end
+        column "User Email", :identity_id do |update|
+          update.identity.user.email # Adjust according to your user association
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
