e3232a549e19c9fef289cc147f293225df5b15bc
Chris Toynbee <ctoynbee@gmail.com>
2024-02-09 10:37:36 +1300

    Fix incorrect sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -25,11 +25,11 @@ if !Rails.env.development? && !Rails.env.test?
   end
 end
 
-if ENV.fetch("IS_SCHEDULER", "false") == "true"
+if ENV.fetch("IS_SCHEDULER", false)
   Sidekiq.configure_server do |config|
-    schedule_file = "config/sidekiq.yml"
-    if File.exist?(schedule_file) && Sidekiq.server?
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    config.on(:startup) do
+      Sidekiq.schedule = YAML.load_file(File.expand_path("../scheduler.yml", File.dirname(__FILE__)))
+      Sidekiq::Scheduler.reload_schedule!
     end
   end
 end
