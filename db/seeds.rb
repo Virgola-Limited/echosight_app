d3518fb2dce0596cb3dc7974d450b16dc15f8341
Chris Toynbee <ctoynbee@gmail.com>
2024-02-09 15:07:20 +1300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@
 #   end
 
 
-  def create_follower_count_data(identity_id, start_date, end_date, start_followers, end_followers)
-    TwitterFollowersCount.where(identity_id: identity_id).destroy_all
-    total_days = (end_date - start_date).to_i
-    daily_increment = (end_followers - start_followers) / total_days
+def create_follower_count_data(identity_id, start_date, end_date, start_followers, end_followers)
+  TwitterFollowersCount.where(identity_id: identity_id).delete_all
+  total_days = (end_date - start_date).to_i
+  daily_increment = (end_followers - start_followers) / total_days.to_f
 
-    (start_date..end_date).each do |date|
-      follower_count = start_followers + (date - start_date).to_i * daily_increment
-      TwitterFollowersCount.create!(
-        identity_id: identity_id,
-        followers_count: follower_count.to_s,
-        date: date
-      )
-    end
+  (start_date..end_date).each do |date|
+    follower_count = start_followers + ((date - start_date).to_i * daily_increment).round
+    TwitterFollowersCount.create!(
+      identity_id: identity_id,
+      followers_count: follower_count,
+      date: date
+    )
   end
+end
 
   # Helper method to create fake Tweet objects
   def create_fake_tweets(identity_id, number_of_tweets)
@@ -37,49 +37,38 @@
   end
 
   # Helper method to create tweet metrics data with gradual increase or decrease
-  def create_tweet_metrics_data(tweet_id, start_date)
-    base_retweet_count = 50
-    base_quote_count = 25
-    base_like_count = 100
-    base_quote_count = 25
-    base_impression_count = 500
-    base_reply_count = 50
-    base_bookmark_count = 50
-    base_user_profile_clicks = 50
+  def create_tweet_metrics_data(identity_id, start_date, end_date, start_clicks, end_clicks)
+    Tweet.where(identity_id: identity_id).find_each do |tweet|
+      daily_increment = (end_clicks - start_clicks) / (end_date - start_date).to_i
+      clicks_accumulator = start_clicks
 
-    7.times do |n|
-      TweetMetric.create!(
-        tweet_id: tweet_id,
-        retweet_count: base_retweet_count + rand(-5..5),
-        quote_count: base_quote_count + rand(-3..3),
-        like_count: base_like_count + rand(-10..10),
-        impression_count: base_impression_count + rand(-50..50),
-        reply_count: base_reply_count + rand(-5..5),
-        bookmark_count: base_bookmark_count + rand(-5..5),
-        user_profile_clicks: base_user_profile_clicks + rand(-5..5),
-        pulled_at: start_date + n.days,
-        created_at: Time.now,
-        updated_at: Time.now
-      )
+      (start_date..end_date).each do |date|
+        clicks_for_day = (clicks_accumulator + daily_increment).round
+        TweetMetric.create!(
+          tweet_id: tweet.id,
+          user_profile_clicks: clicks_for_day,
+          pulled_at: date.to_time(:utc),
+          retweet_count: rand(5..15),
+          quote_count: rand(3..12),
+          like_count: rand(20..50),
+          reply_count: rand(1..10),
+          bookmark_count: rand(5..10),
+          impression_count: rand(100..300)
+        )
 
-      # Update base values for the next day
-      base_retweet_count += rand(-2..2)
-      base_quote_count += rand(-1..1)
-      base_like_count += rand(-5..5)
-      base_impression_count += rand(-25..25)
-      base_reply_count += rand(-2..2)
-      base_bookmark_count += rand(-2..2)
-      base_user_profile_clicks += rand(-2..2)
+        clicks_accumulator = clicks_for_day
+      end
     end
   end
 
+
   # Days required in follower seed data
-  days_required_in_follower_seed_data = 7.days.ago.to_date
+  # days_required_in_follower_seed_data = 7.days.ago.to_date
   # days_required_in_follower_seed_data = 14.days.ago.to_date unless days_required_in_follower_seed_data
   # days_required_in_follower_seed_data = 31.days.ago.to_date unless days_required_in_follower_seed_data
-  days_required_in_follower_seed_data = 60.days.ago.to_date unless days_required_in_follower_seed_data
-  days_required_in_follower_seed_data = 6.months.ago.to_date unless days_required_in_follower_seed_data
-  days_required_in_follower_seed_data = 13.months.ago.to_date unless days_required_in_follower_seed_data
+  # days_required_in_follower_seed_data = 60.days.ago.to_date unless days_required_in_follower_seed_data
+  # days_required_in_follower_seed_data = 6.months.ago.to_date unless days_required_in_follower_seed_data
+  # days_required_in_follower_seed_data = 13.months.ago.to_date unless days_required_in_follower_seed_data
 
 
 if Rails.env.development?
@@ -95,17 +84,28 @@ if Rails.env.development?
     tweets = Tweet.where(identity_id: identity_id)
 
     # Generate 7 days worth of metrics for each tweet
-    tweets.each do |tweet|
-      start_date = 7.days.ago.to_date
-      create_tweet_metrics_data(tweet.id, start_date)
-    end
+    # tweets.each do |tweet|
+    #   start_date = 7.days.ago.to_date
+    #   create_tweet_metrics_data(tweet.id, start_date)
+    # end
 
-    if days_required_in_follower_seed_data
+    # if days_required_in_follower_seed_data
       # Identity ID from the first user's identity
-      identity_id = User.first.identity.id
-      TwitterFollowersCount.where(identity_id: identity_id).delete_all
-      create_follower_count_data(identity_id, days_required_in_follower_seed_data, Date.today, 1000, 1070)
-    end
+      # identity_id = User.first.identity.id
+      # TwitterFollowersCount.where(identity_id: identity_id).delete_all
+      # create_follower_count_data(identity_id, days_required_in_follower_seed_data, Date.today, 1000, 1070)
+    # end
+
+    start_date = 7.days.ago.to_date
+    end_date = Date.today
+    start_followers = 1000
+    end_followers = 1070
+    start_clicks = 10
+    end_clicks = 70
+
+    create_follower_count_data(identity_id, start_date, end_date, start_followers, end_followers)
+    create_tweet_metrics_data(identity_id, start_date, end_date, start_clicks, end_clicks)
+
   end
 
   AdminUser.create(email: 'ctoynbee@gmail.com', password: 'nittfagm', password_confirmation: 'nittfagm')
