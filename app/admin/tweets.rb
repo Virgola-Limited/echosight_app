b6344552da97426839f85f1431a843645a1df724
Chris Toynbee <ctoynbee@gmail.com>
2024-02-15 16:05:33 +1300

    Add user to admin tweets page

--- app/admin/tweets.rb
+++ app/admin/tweets.rb
@@ -6,6 +6,13 @@ ActiveAdmin.register Tweet do
     column "Tweet" do |tweet|
       link_to tweet.text.truncate(50), "https://twitter.com/#{tweet.identity.handle}/status/#{tweet.twitter_id}", target: "_blank"
     end
+    column "User Email" do |tweet|
+      if tweet.identity && tweet.identity.user
+        link_to tweet.identity.user.email, admin_user_path(tweet.identity.user)
+      else
+        "No User"
+      end
+    end
     column :created_at
     column :updated_at
     actions
