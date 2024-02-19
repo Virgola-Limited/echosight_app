e05a6a7d4023517ac2e2b5a10d66f603550436ce
Chris Toynbee <ctoynbee@gmail.com>
2024-02-19 19:05:46 +1300

    Dont refresh tweets older than 30 days

--- app/services/twitter/tweet_metrics_refresher.rb
+++ app/services/twitter/tweet_metrics_refresher.rb
@@ -12,25 +12,26 @@ module Twitter
 
     def call
       # Process tweets less than 30 days old
-      outdated_tweet_ids(recent: true).each_slice(BATCH_SIZE) do |batch|
+      outdated_tweet_ids.each_slice(BATCH_SIZE) do |batch|
         update_tweets_and_metrics(batch, include_non_public_metrics: true)
       end
 
       # Process tweets older than 30 days
-      outdated_tweet_ids(recent: false).each_slice(BATCH_SIZE) do |batch|
-        update_tweets_and_metrics(batch, include_non_public_metrics: false)
-      end
+      # Dean says we dont need this and we are hitting limits on the basic plan
+      # outdated_tweet_ids(recent: false).each_slice(BATCH_SIZE) do |batch|
+      #   update_tweets_and_metrics(batch, include_non_public_metrics: false)
+      # end
     end
 
     private
 
-    def outdated_tweet_ids(recent: true)
+    def outdated_tweet_ids#(recent: true)
       scope = user.identity.tweets.order(updated_at: :asc)
-      if recent
+      # if recent
         scope.where('twitter_created_at > ?', 30.days.ago).pluck(:twitter_id)
-      else
-        scope.where('twitter_created_at <= ?', 30.days.ago).pluck(:twitter_id)
-      end
+      # else
+        # scope.where('twitter_created_at <= ?', 30.days.ago).pluck(:twitter_id)
+      # end
     end
 
     def update_tweets_and_metrics(tweet_ids, include_non_public_metrics: true)
