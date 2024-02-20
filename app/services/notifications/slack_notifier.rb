8d77da92d27a8fce4fdef0c455336bc761a9ed4a
Chris Toynbee <ctoynbee@gmail.com>
2024-02-21 11:22:02 +1300

    Fix notifier bug

--- app/services/notifications/slack_notifier.rb
+++ app/services/notifications/slack_notifier.rb
@@ -1,12 +1,9 @@
 module Notifications
   class SlackNotifier
-    def initialize(channel: nil)
-      channel = channel || '#default'
-      @notifier = Slack::Notifier.new(Rails.application.credentials.slack[:webhook_url], channel: channel)
-    end
-
     def self.call(message:, channel: nil)
-      self.new(channel: nil).ping(message)
+      channel = channel || '#default'
+      notifier = Slack::Notifier.new(Rails.application.credentials.slack[:webhook_url], channel: channel)
+      notifier.ping(message)
     end
   end
 end
