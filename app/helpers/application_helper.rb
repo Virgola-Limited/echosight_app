b5cb92bf0cabb622d6d6ca7ad3a337b3ffdd4b19
Chris Toynbee <ctoynbee@gmail.com>
2024-02-23 12:32:56 +1300

    Add bio link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  include ActionView::Helpers::UrlHelper
+
+  def html_description_with_links(description)
+    description.to_s.gsub(/(https?:\/\/[^\s]+)/) do |url|
+      link_to(url, url, target: '_blank', rel: 'noopener noreferrer')
+    end.html_safe
+  end
 end
