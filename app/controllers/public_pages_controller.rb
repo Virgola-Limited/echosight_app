343e3387cc441eefd4ed9f89f5688a0574e07736
Chris Toynbee <ctoynbee@gmail.com>
2024-02-23 11:30:30 +1300

    Fix incorrect debug

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -115,7 +115,7 @@ class PublicPagesController < ApplicationController
     ############################
     # Engagement Graph
     @engagement_rate_percentage_per_day = tweet_metrics_query.engagement_rate_percentage_per_day
-    Rails.logger.debug('paul' + @engagement_rate_percentage_per_day.inspect)
+    Rails.logger.info('paul' + @engagement_rate_percentage_per_day.inspect)
     # force build on heroku
 
     ############################
