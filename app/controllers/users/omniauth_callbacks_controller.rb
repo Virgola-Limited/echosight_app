64e7a6b2d2f8fb86934df47e3cd2181ba69ee3bf
Chris Toynbee <ctoynbee@gmail.com>
2024-02-25 17:20:07 +1300

    Handle user trying to connect to twitter when they have already connected on another account

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ module Users
         # Scenario 1: User is already signed in with Devise
         @user = current_user
         @user.update_identity_from_auth(auth)
-        redirect_to dashboard_index_path
+        redirect_to dashboard_index_path, notice: 'Your Twitter account has been successfully connected.'
       else
         # Scenario 2: User is signing up for the first time with Twitter
         @user = User.create_or_update_identity_from_omniauth(auth)
@@ -21,6 +21,12 @@ module Users
           session['devise.twitter_data'] = auth.except('extra')
           redirect_to new_user_registration_url
         end
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      if e.record.errors[:uid].include?('has already been taken') || e.record.errors[:handle].include?('has already been taken')
+        redirect_to dashboard_index_path, alert: "This Twitter account is already connected to an existing Echosight account. Please log in with that account or contact support for assistance."
+      else
+        raise e # Re-raise the exception if it's not related to UID or Handle uniqueness
       end
     end
   end
