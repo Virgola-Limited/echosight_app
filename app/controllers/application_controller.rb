9155342d1c36a641298ab138b065a23fa1e3a2c7
Chris Toynbee <ctoynbee@gmail.com>
2024-02-27 16:11:44 +1300

    Disable staging auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_staging
-    return unless Rails.env.staging?
+    # return unless Rails.env.staging?
 
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['SITE_USERNAME'] && password == ENV['SITE_PASSWORD']
-    end
+    # authenticate_or_request_with_http_basic do |username, password|
+    #   username == ENV['SITE_USERNAME'] && password == ENV['SITE_PASSWORD']
+    # end
   end
 
   def current_or_guest_user
