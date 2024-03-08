6d3030a00c61d916c71db0185abb8f8a2be469e9
Chris Toynbee <ctoynbee@gmail.com>
2024-03-08 15:03:04 +1300

    disable slack option

--- app/services/notifications/slack_notifier.rb
+++ app/services/notifications/slack_notifier.rb
@@ -1,6 +1,8 @@
 module Notifications
   class SlackNotifier
     def self.call(message:, channel: :general)
+      return if ENV['DISABLE_SLACK_NOTIFY']
+
       webhook_urls = Rails.application.credentials.slack[:webhook_url]
       webhook_url = webhook_urls[channel] || webhook_urls[:general]
 
