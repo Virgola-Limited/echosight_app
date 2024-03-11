64f9d16d15e359c8887798d8e615d052e520c577
Chris Toynbee <ctoynbee@gmail.com>
2024-03-11 16:28:08 +1300

    test to ensure tweet and tweet metrics is unique

--- spec/queries/twitter/tweet_metrics_query_spec.rb
+++ spec/queries/twitter/tweet_metrics_query_spec.rb
@@ -6,17 +6,18 @@ RSpec.describe Twitter::TweetMetricsQuery do
     let(:user) { create(:user, identity: identity) }
     let!(:tweets) do
       [
-        create(:tweet, identity: identity, created_at: 6.days.ago).tap do |tweet|
+        create(:tweet, identity: identity, twitter_created_at: 6.days.ago).tap do |tweet|
           create(:tweet_metric, tweet: tweet, impression_count: 500)
         end,
-        create(:tweet, identity: identity, created_at: 5.days.ago).tap do |tweet|
+        create(:tweet, identity: identity, twitter_created_at: 5.days.ago).tap do |tweet|
           create(:tweet_metric, tweet: tweet, impression_count: 1500)
         end,
-        create(:tweet, identity: identity, created_at: 4.days.ago).tap do |tweet|
+        create(:tweet, identity: identity, twitter_created_at: 4.days.ago).tap do |tweet|
+          create(:tweet_metric, tweet: tweet, impression_count: 200)
           create(:tweet_metric, tweet: tweet, impression_count: 300)
         end,
         # keep this to ensure we dont get weird results with nil impression_count
-        create(:tweet, identity: identity, created_at: 3.days.ago).tap do |tweet|
+        create(:tweet, identity: identity, twitter_created_at: 3.days.ago).tap do |tweet|
           create(:tweet_metric, tweet: tweet, impression_count: nil)
         end
       ]
@@ -25,9 +26,9 @@ RSpec.describe Twitter::TweetMetricsQuery do
     it 'returns tweets sorted by impression_count in descending order' do
       query = described_class.new(user: user)
       top_tweets = query.top_tweets_for_user
-
-      expect(top_tweets.map(&:id)).to eq([tweets[1].id, tweets[0].id, tweets[2].id])
-      expect(top_tweets.map(&:impression_count)).to eq([1500, 500, 300])
+      expect(top_tweets.map(&:id)).to match_array(top_tweets.map(&:id).uniq)
+      # byebug
+      expect(top_tweets.flat_map(&:tweet_metrics).map(&:impression_count)).to eq([1500, 500, 300])
     end
   end
 end
