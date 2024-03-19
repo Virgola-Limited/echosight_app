52a6b4e197a5625a0b5435280b69e7ac63548639
Chris Toynbee <ctoynbee@gmail.com>
2024-03-19 18:15:10 +1300

    Fix blurry profile image

--- app/services/user_updater.rb
+++ app/services/user_updater.rb
@@ -8,11 +8,39 @@ class UserUpdater
   end
 
   def call
-    if  user_data['image_url'].nil? || user_data['banner_url'].nil?
-      raise ArgumentError, "User data must include image_url and banner_url: #{user_data}"
-    end
+    raise ArgumentError, "User data must include image_url and banner_url: #{user_data}" if user_data['image_url'].nil? || user_data['banner_url'].nil?
 
     identity = Identity.find_by!(handle: user_data['username'])
-    identity.update!(image_url: user_data['image_url'], banner_url: user_data['banner_url'])
+    updated_image_url = transform_image_url(user_data['image_url'])
+    identity.update!(image_url: updated_image_url, banner_url: user_data['banner_url'])
+  end
+
+  private
+
+  def transform_image_url(url)
+    if image_link?(url.gsub('_normal', '_400x400'))
+      url.gsub('_normal', '_400x400')
+    else
+      url
+    end
+  end
+
+  def image_link?(url)
+    uri = URI.parse(url)
+    use_ssl = uri.scheme == 'https'
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl, open_timeout: 5, read_timeout: 5) do |http|
+      response = http.request_head(uri.path.blank? ? '/' : uri.path)
+
+      # Handling redirects, considering only a single redirect for simplicity
+      if response.code.to_i >= 300 && response.code.to_i < 400 && response['location'].present?
+        return image_link?(response['location'])
+      end
+
+      response.content_type&.start_with?('image/')
+    end
+  rescue => e
+    Rails.logger.error("Error checking image link: #{e.message}")
+    false
   end
 end
--- spec/services/user_updater_spec.rb
+++ spec/services/user_updater_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe UserUpdater do
-  subject { described_class.new }
-
+RSpec.describe UserUpdater, :vcr do
   describe '#call' do
     let!(:identity) { create(:identity, :loftwah, :with_oauth_credential) }
     let(:user_data) do
       {
-        "id"=>"1192091185",
+        "id" => "1192091185",
         "username" => identity.handle,
         "public_metrics" => {
           "followers_count" => 6676,
@@ -15,17 +13,29 @@ RSpec.describe UserUpdater do
           "listed_count" => 42,
           "tweet_count" => 52729
         },
-        "image_url" => 'https://pbs.twimg.com/profile_images/1756873036220059648/zc13kjbX_normal.jpg',
+        "image_url" => image_url,
         "banner_url" => "https://pbs.twimg.com/profile_banners/1192091185/1707817030"
       }
     end
     let(:updater) { described_class.new(user_data) }
+    let(:image_url) { 'https://pbs.twimg.com/profile_images/1756873036220059648/zc13kjbX_normal.jpg' }
 
-    it 'updates the user images' do
-      expect(identity.banner_url).to be_nil
-      expect { updater.call }.to change { identity.reload.image_url }.from(nil).to(user_data["image_url"])
-      expect(identity.banner_url).to eq(user_data["banner_url"])
+    context 'when the 400x400 image exists' do
+      let(:expected_image_url) { user_data["image_url"].gsub('_normal', '_400x400') }
+
+      it 'updates the user images with transformed URL' do
+        expect(identity.banner_url).to be_nil
+        expect { updater.call }.to change { identity.reload.image_url }.from(nil).to(expected_image_url)
+        expect(identity.banner_url).to eq(user_data["banner_url"])
+      end
     end
 
+    context 'when the 400x400 image does not exist' do
+      it 'update the user image with the original URL' do
+        expect(identity.banner_url).to be_nil
+        expect { updater.call }.to change { identity.reload.image_url }.from(nil).to(image_url)
+        expect(identity.banner_url).to eq(user_data["banner_url"])
+      end
+    end
   end
 end
