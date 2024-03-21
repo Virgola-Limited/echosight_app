396ee44bf7262a49a175f4715765338fe56bd91f
Chris <570876+tophertoy@users.noreply.github.com>
2024-03-22 08:37:00 +1300

    feat: Reorder graphs, round numbers, refactor public page into crude service

---
+++ app/services/public_page_service.rb
@@ -0,0 +1,236 @@
+class PublicPageService < Services::Base
+  attr_reader :user, :current_admin_user
+  def initialize(user:, current_admin_user:)
+    @user = user
+    @current_admin_user = current_admin_user
+  end
+
+  def call
+    @maximum_days_of_data = Twitter::TweetMetricsQuery.maximum_days_of_data
+
+    ############################
+    # Posts/Tweet Counts
+    @tweet_count_over_available_time_period = tweet_metrics_query.tweet_count_over_available_time_period
+    @tweets_change_over_available_time_period = tweet_metrics_query.tweets_change_over_available_time_period
+    @tweet_comparison_days = tweet_metrics_query.tweet_comparison_days
+    if @tweets_change_over_available_time_period > 0
+      @tweets_change_over_available_time_period = "#{@tweets_change_over_available_time_period} increase"
+    elsif @tweets_change_over_available_time_period < 0
+      @tweets_change_over_available_time_period = "#{@tweets_change_over_available_time_period.abs} decrease"
+    else
+      @tweets_change_over_available_time_period = 'No change'
+    end
+
+    ############################
+    # Impressions
+    @impressions_count = tweet_metrics_query.impressions_count
+    @impressions_change_since_last_week = tweet_metrics_query.impressions_change_since_last_week
+    if @impressions_change_since_last_week
+      NumberRoundingService.round_number(@impressions_change_since_last_week)
+      if @impressions_change_since_last_week > 0
+        @impressions_change_since_last_week = "#{@impressions_change_since_last_week}% increase"
+      elsif @impressions_change_since_last_week < 0
+        @impressions_change_since_last_week = "#{@impressions_change_since_last_week.abs}% decrease"
+      else
+        @impressions_change_since_last_week = 'No change'
+      end
+    end
+
+    # this needs to change to be dynamic
+    @impressions_comparison_days = 7
+
+    ############################
+    # Likes Counts
+    @likes_count = tweet_metrics_query.likes_count
+    @likes_change_since_last_week = tweet_metrics_query.likes_change_since_last_week
+    if @likes_change_since_last_week
+      NumberRoundingService.round_number(@likes_change_since_last_week)
+      if @likes_change_since_last_week > 0
+        @likes_change_since_last_week = "#{@likes_change_since_last_week}% increase"
+      elsif @likes_change_since_last_week < 0
+        @likes_change_since_last_week = "#{@likes_change_since_last_week.abs}% decrease"
+      else
+        @likes_change_since_last_week = 'No change'
+      end
+    end
+
+    @likes_comparison_days = 7
+
+    ############################
+    # Followers Counts
+    @followers_count = followers_query.followers_count
+    @followers_count_change_percentage_text = followers_query.followers_count_change_percentage
+
+
+    # this needs to change to be dynamic
+    @followers_comparison_days = 7
+
+    ############################
+    # Followers Graph
+    formatted_follower_data, follower_daily_data_points = followers_query.followers_data_for_graph
+    @follower_formatted_labels_for_graph = formatted_follower_data
+    @follower_daily_data_points_for_graph = follower_daily_data_points
+
+    ############################
+    # Engagement Graph
+    @engagement_rate_percentage_per_day = tweet_metrics_query.engagement_rate_percentage_per_day
+
+    ############################
+    # Impressions over Time Graph
+    impression_counts_per_day = tweet_metrics_query.impression_counts_per_day
+    # Admin only data
+    if current_admin_user
+      @first_day_impressions = tweet_metrics_query.first_day_impressions
+      @first_impressions_message = ''
+      if @first_day_impressions
+        @first_impressions_message = "Based on #{@first_day_impressions[:impression_count].to_s} on #{@first_day_impressions[:date].to_s} "
+      end
+    end
+
+    @impression_formatted_labels_for_graph = tweet_metrics_query.impression_counts_per_day.map do |data|
+      label = data[:date].strftime('%b %d')
+      label += " (#{data[:impression_count]})" if current_admin_user.present?
+      label
+    end
+
+    @impression_daily_data_points_for_graph = tweet_metrics_query.impression_counts_per_day.map do |data|
+      data[:impression_count] >= 0 ? data[:impression_count] : 0
+    end
+    # Rails.logger.debug('paul' + @impression_daily_data_points_for_graph.inspect)
+
+    ############################
+    # Profile Conversion Rate
+    # @profile_clicks_data = tweet_metrics_query.profile_clicks_count_per_day
+    # @followers_data = followers_query.daily_followers_count
+    # @conversion_rates_data_for_graph = profile_conversion_rate_query.conversion_rates_data_for_graph(profile_clicks_data: @profile_clicks_data, followers_data: @followers_data)
+
+    ############################
+    # Top Posts / Tweets
+    @top_posts = tweet_metrics_query.top_tweets_for_user
+
+    ############################
+
+    results
+  end
+
+  private
+
+  PublicPageResults = Struct.new(
+    :engagement_rate_percentage_per_day,
+    :first_day_impressions,
+    :first_impressions_message,
+    :follower_daily_data_points_for_graph,
+    :follower_formatted_labels_for_graph,
+    :followers_comparison_days,
+    :followers_count,
+    :followers_count_change_percentage_text,
+    :impression_daily_data_points_for_graph,
+    :impression_formatted_labels_for_graph,
+    :impressions_change_since_last_week,
+    :impressions_comparison_days,
+    :impressions_count,
+    :likes_change_since_last_week,
+    :likes_comparison_days,
+    :likes_count,
+    :maximum_days_of_data,
+    :top_posts,
+    :tweet_comparison_days,
+    :tweet_count_over_available_time_period,
+    :tweets_change_over_available_time_period
+  )
+
+  ROUNDABLE_METRICS = %i[
+    impressions_count
+    impressions_change_since_last_week
+    likes_count
+    followers_count
+].freeze
+
+  def results
+
+    ROUNDABLE_METRICS.each do |metric|
+      instance_variable_set("@#{metric}", number_rounding_service.round_number(instance_variable_get("@#{metric}")))
+    end
+    PublicPageResults.new(
+      @engagement_rate_percentage_per_day,
+      @first_day_impressions,
+      @first_impressions_message,
+      @follower_daily_data_points_for_graph,
+      @follower_formatted_labels_for_graph,
+      @followers_comparison_days,
+      @followers_count,
+      @followers_count_change_percentage_text,
+      @impression_daily_data_points_for_graph,
+      @impression_formatted_labels_for_graph,
+      @impressions_change_since_last_week,
+      @impressions_comparison_days,
+      @impressions_count,
+      @likes_change_since_last_week,
+      @likes_comparison_days,
+      @likes_count,
+      @maximum_days_of_data,
+      @top_posts,
+      @tweet_comparison_days,
+      @tweet_count_over_available_time_period,
+      @tweets_change_over_available_time_period
+    )
+  end
+
+  def profile_conversion_rate_query
+    Twitter::ProfileConversionRateQuery.new
+  end
+
+  def tweet_metrics_query
+    Twitter::TweetMetricsQuery.new(user: user)
+  end
+
+  def followers_query
+    Twitter::TwitterUserMetricsQuery.new(user)
+  end
+
+  def number_rounding_service
+    NumberRoundingService
+  end
+end
+
+
+
+# TODO move this to lib
+class NumberRoundingService
+  def self.round_number(input)
+    Rails.logger.debug('paul' + input.inspect)
+    return input if input === false
+    number = extract_number(input)
+    sign = number.negative? ? '-' : ''
+    number = number.round.abs
+
+    rounded_number = if number < 999
+                       number
+                     elsif number < 1_000_000
+                       "#{(number / 1_000.0).round}K"
+                     else
+                       "#{format('%.2f', number / 1_000_000.0)}M"
+                     end
+
+    format_output(sign, rounded_number, input)
+  end
+
+  private
+
+  def self.extract_number(input)
+    if input.is_a?(String) && input.include?('%')
+      input.to_f
+    else
+      input
+    end
+  end
+
+  def self.format_output(sign, rounded_number, original_input)
+    if original_input.is_a?(String) && original_input.include?('%')
+      "#{sign}#{rounded_number}% #{original_input.split.last}"
+    else
+      "#{sign}#{rounded_number}"
+    end
+  end
+end
+
