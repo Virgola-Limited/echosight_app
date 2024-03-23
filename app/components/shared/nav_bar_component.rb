ff69a950e2fbd65bb096f7458475166d7f8edab4
Chris Toynbee <ctoynbee@gmail.com>
2024-03-24 12:02:18 +1300

    Fix mobile menu

--- app/components/shared/nav_bar_component.rb
+++ app/components/shared/nav_bar_component.rb
@@ -9,6 +9,12 @@ module Shared
     def demo_or_real_public_page_link
       # <a target="_blank" href="<%= current_or_guest_user.handle ? public_page_url(current_or_guest_user.handle) : '#' %>" class="block rounded text-primary-700 dark:text-primary-500" aria-current="page">My Public Page</a>
 
+    def nav_links
+      [
+        { name: 'Dashboard', path: root_url },
+        { name: 'My Public Page', path: demo_or_real_public_page_link },
+        { name: 'My Profile & Settings', path: edit_user_registration_path }
+      ]
     end
 
   end
