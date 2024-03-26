5395f31910aaf57b2d29e3337f5156338aaf9ad6
Chris Toynbee <ctoynbee@gmail.com>
2024-03-27 10:10:18 +1300

    Fix broken flash message

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -3,32 +3,17 @@
 class PublicPagesController < ApplicationController
   include Cacheable
 
-  # if handle its the public url
-  # if no handle and logged in its them looking at their own page so dont 404
-  # show either their page or demo page
-
-  # alway show the current users page if the user is my_public_page?
-  # Only make the url my_public_page if they havent connected to their twitter account. (no identity?)
   def show
     if params[:handle] == 'demo' && current_user&.identity.present?
       redirect_to public_page_path(current_user.identity.handle) and return
     end
 
     public_page_data = PublicPageService.call(handle: params[:handle], current_user: current_or_guest_user, current_admin_user: current_admin_user)
-    if public_page_data.demo?
-      flash[:notice] = demo_page_flash_message
+    if public_page_data.demo? && current_or_guest_user.guest?
+      sign_up_link = view_context.link_to('Sign up', new_user_registration_path).html_safe
+      flash.now[:notice] = "This is a demo page showing how your public page could look. #{sign_up_link} to get your own!".html_safe
     end
 
-      render PublicPageComponent.new(public_page_data: public_page_data, current_user: current_or_guest_user)
-    # end
+    render PublicPageComponent.new(public_page_data: public_page_data, current_user: current_or_guest_user)
   end
-
-  def demo_page_flash_message
-    if current_or_guest_user.guest?
-      sign_up_link = view_context.link_to('Sign up', new_user_registration_path)
-      flash[:notice] = "This is a demo page showing how your public page could look. #{sign_up_link} to get your own!".html_safe
-    end
-  end
-
-
 end
