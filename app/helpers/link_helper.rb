b37822501988e1e9391fdaf548e5729b3bd0e8a1
Chris Toynbee <ctoynbee@gmail.com>
2024-03-27 08:32:50 +1300

    Remove link image generation code

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -9,29 +9,8 @@ module LinkHelper
   def text_with_images(text)
     processed_text = text.dup
     processed_text.gsub!(URI.regexp(['http', 'https'])) do |url|
-      if image_link?(url)
-        "<img src='#{url}' alt='Tweet Image' style='max-width:100%;'/>"
-      else
-        link_to(url, url, target: "_blank")
-      end
+      link_to(url, url, target: "_blank")
     end
     processed_text.html_safe
-  end
-
-  private
-
-  ###########################################################
-  # MVP: SUPER SLOW DO NOT USE IN PRODUCTION
-  # TODO need to background this before we go into production
-  ###########################################################
-  def image_link?(url)
-    uri = URI.parse(url)
-    request = Net::HTTP.new(uri.host, uri.port)
-    request.use_ssl = (uri.scheme == 'https')
-    response = request.request_head(uri.path.empty? ? "/" : uri.path)
-    response.content_type&.start_with?('image/')
-  rescue => e
-    Rails.logger.error("Error checking image link: #{e.message}")
-    false
   end
 end
