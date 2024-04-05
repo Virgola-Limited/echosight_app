93dce10c46798a4d96a016dcec6acf4ddf5fc6f6
Chris Toynbee <ctoynbee@gmail.com>
2024-04-06 08:30:46 +1300

    Fix test to ensure tweet metric not created twice in the same day

--- app/services/twitter/tweet_and_metric_upserter.rb
+++ app/services/twitter/tweet_and_metric_upserter.rb
@@ -8,40 +8,56 @@ module Twitter
     end
 
     def call
-      metrics = tweet_data['public_metrics']
-      non_public_metrics = tweet_data['non_public_metrics']
+      tweet = initialize_or_update_tweet
+      tweet_metric = find_or_initialize_tweet_metric(tweet)
+
+      [update_tweet_metric(tweet_metric), tweet]
+    end
+
+    private
+
+    def initialize_or_update_tweet
       tweet = Tweet.find_or_initialize_by(twitter_id: tweet_data['id'])
       # if tweet.new_record?
         # Schedule the metrics update job to run 24 hours after the tweet's creation
         # Twitter::TweetMetricsUpdateJob.perform_in(24.hours, tweet.id)
       # end
-      twitter_created_at = DateTime.parse(tweet_data['created_at'])
+      tweet.assign_attributes(tweet_attributes)
+      tweet.save! if tweet.new_record? || tweet.changed?
+      tweet
+    end
 
-      tweet_updated = tweet.new_record? || tweet.changed?
-      tweet.assign_attributes(
+    def tweet_attributes
+      {
         text: tweet_data['text'],
         identity_id: user.identity.id,
-        twitter_created_at: twitter_created_at
-      )
-      tweet.save! if tweet_updated
+        twitter_created_at: DateTime.parse(tweet_data['created_at'])
+      }
+    end
 
-      metric_attributes = {
+    def find_or_initialize_tweet_metric(tweet)
+      # Use a date range for today to find an existing TweetMetric or initialize a new one
+      today_range = DateTime.current.beginning_of_day..DateTime.current.end_of_day
+      TweetMetric.find_or_initialize_by(tweet: tweet, pulled_at: today_range)
+    end
+
+    def update_tweet_metric(tweet_metric)
+      tweet_metric.assign_attributes(metric_attributes)
+      tweet_metric.pulled_at = DateTime.current unless tweet_metric.persisted?
+      tweet_metric.save!
+    end
+
+    def metric_attributes
+      metrics = tweet_data['public_metrics']
+      {
         retweet_count: metrics['retweet_count'].to_i,
         quote_count: metrics['quote_count'].to_i,
         like_count: metrics['like_count'].to_i,
         impression_count: metrics['impression_count'].to_i,
         reply_count: metrics['reply_count'].to_i,
-        bookmark_count: metrics['bookmark_count'].to_i,
+        bookmark_count: metrics['bookmark_count'].to_i
+        # Add non_public_metrics if needed
       }
-
-      if non_public_metrics && non_public_metrics['user_profile_clicks']
-        metric_attributes.merge!(
-          user_profile_clicks: non_public_metrics['user_profile_clicks']
-        )
-      end
-      tweet_metric = TweetMetric.find_or_initialize_by(tweet: tweet, pulled_at: DateTime.current)
-
-      [tweet_metric.update!(metric_attributes), tweet_updated]
     end
   end
 end
