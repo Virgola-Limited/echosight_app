2428d44df9ded57b93b976dbb5aeab7ddc559ee9
Chris Toynbee <ctoynbee@gmail.com>
2024-04-20 18:30:23 +1200

    Archive old existing tweets updater

---
+++ spec/services/twitter/existing_tweets_updater_old_spec.rb
@@ -0,0 +1,172 @@
+require 'rails_helper'
+
+RSpec.describe Twitter::ExistingTweetsUpdaterOld do
+  let!(:user) { create(:user, :with_identity, confirmed_at: 1.day.ago) }
+  let(:client) { double('SocialData::ClientAdapter') }
+  let(:subject) { described_class.new(user: user, client: client) }
+  # let(:tweet) { create(:tweet, id: 1770016891555496274, identity: user.identity, twitter_created_at: 4.hour.ago)}
+  let(:updatable_tweet) { create(:tweet, id: 1765212190418899365, identity: user.identity, twitter_created_at: Time.current)}
+  # this is 1 second before the unix timestamp of the since id and and 1 second later than the max id
+  let(:expected_query) { { query: "from:#{user.identity.handle} since_time:1709694355 until_time:1709694357" } }
+
+  describe '#call' do
+    before do
+      # allow(Twitter::UserMetricsUpdater).to receive(:new).and_return(double(call: nil))
+      allow(IdentityUpdater).to receive(:new).and_return(double(call: nil))
+    end
+
+    after do
+      travel_back
+    end
+
+    it 'does not update the metric in the first 24 hours' do
+      create(:tweet_metric, tweet: updatable_tweet, pulled_at: Time.current)
+      expect(TweetMetric.count).to eq(1)
+      expect(client).not_to receive(:search_tweets)
+      subject.call
+
+      1.upto(24) do |hour|
+        travel_to(hour.hours.from_now) do
+          begin
+            expect(client).not_to receive(:search_tweets)
+            expect(Notifications::SlackNotifier).to receive(:call)
+            subject.call
+          rescue RSpec::Mocks::MockExpectationError => e
+            raise "#{hour} didn't expect to call search_tweets. Original error: #{e.message}"
+          end
+        end
+      end
+      expect(TweetMetric.count).to eq(1)
+    end
+
+    let(:data_response) do
+      [
+        {
+        'id' => '1765212190418899365',
+        'text' => 'to come to',
+        'created_at' => '2024-03-06T03:05:56.000000Z',
+        'public_metrics' => {
+          'like_count' => 0,
+          'quote_count' => 0,
+          'reply_count' => 0,
+          'retweet_count' => 0,
+          'impression_count' => 11,
+          'bookmark_count' => 0
+        },
+        'is_pinned' => 'false',
+        'user' => {
+          'data' => {
+            'id' => '1691930809756991488',
+            'name' => 'Topher',
+            'username' => 'TopherToy',
+            'description' => 'Twitter/X analytics with Echosight https://t.co/uZpeIYc5Nq',
+            'public_metrics' => {
+              'followers_count' => 13,
+              'following_count' => 21,
+              'listed_count' => 0,
+              'tweet_count' => 40
+            },
+            'image_url' => 'https://pbs.twimg.com/profile_images/1770204882819223552/vrBPzd16_normal.jpg',
+            'banner_url' => 'https://pbs.twimg.com/profile_banners/1691930809756991488/1710884709'
+          }
+        }
+      }
+      ]
+    end
+
+    fit 'updates the metric again after another 24 hours' do
+      initial_tweet_metric = create(:tweet_metric, tweet: updatable_tweet, pulled_at: Time.current)
+
+      travel_to(24.5.hours.from_now) do
+        expect(client).to receive(:search_tweets).with(expected_query).and_return({ 'data' => data_response })
+        expect(Twitter::TweetAndMetricUpserter).to receive(:call).with(tweet_data: data_response, user: user).and_call_original
+        p subject.call
+
+
+
+      end
+      tweet_metric = updatable_tweet.tweet_metrics.last
+      # p TweetMetric.count
+      # expect(updatable_tweet.reload.tweet_metrics.count).to eq(2)
+      # expect(initial_tweet_metric.reload.first_day_pulled_at).to be_nil
+      # expect(tweet_metric.complete_first_day_pulled_at).not_to be_nil
+    end
+
+    before do
+      allow(client).to receive(:search_tweets).with(expected_query).and_return({ 'data' => data_response })
+    end
+
+    it 'updates the metric again after another 48 hours' do
+      p "initial count" + updatable_tweet.tweet_metrics.count.inspect
+      create(:tweet_metric, tweet: updatable_tweet, pulled_at: Time.current)
+      p " now#{Time.current}"
+      # p updatable_tweet.tweet_metrics.count
+      travel_to(24.5.hours.from_now) do
+        p " 24.5 hours from now #{Time.current}"
+        # p '**should save tweet metric first'
+        create(:tweet_metric, tweet: updatable_tweet, pulled_at: Time.current)
+      end
+
+      p "updatable_tweet.tweet_metrics.count after 24 hours" + updatable_tweet.tweet_metrics.count.inspect
+
+      p updatable_tweet.tweet_metrics.last
+
+
+      25.upto(55) do |hour|
+        travel_to(hour.hours.from_now) do
+          p "hour #{hour} from now #{Time.current}"
+          # p '**should save tweet metric second'
+          p DateTime.current
+          # expect(client).to receive(:search_tweets).with(expected_query).and_return({ 'data' => data_response })
+          subject.call
+        end
+      end
+
+      # expect(updatable_tweet.tweet_metrics.count).to eq(3)
+    end
+    context 'when the tweet is more than 14 days old' do
+
+      let!(:old_tweet) { create(:tweet, identity: user.identity, twitter_created_at: 15.days.ago) }
+      let!(:old_tweet_metric) { create(:tweet_metric, tweet: old_tweet, pulled_at: 14.days.ago) }
+
+      it 'does not update more than 14 days' do
+        travel_to(15.days.from_now) do
+          expect(client).not_to receive(:search_tweets)
+          subject.call
+        end
+      end
+    end
+
+    # need to ensure that the tweet can be marked as not pullable? (deleting it could be dangerous)
+    # it 'need to test when a tweet is deleted it might mean we pull too many tweets' do
+    #   pending
+    # end
+
+    # it 'need a test to check if the number of results is the same as the number of tweets' do
+    #   pending
+    # end
+
+    # TODO: needs to check hourly over 24 - 48 hours
+    context 'when handling a mix of new and old tweets' do
+      let(:old_tweet) { create(:tweet, identity: user.identity, twitter_created_at: 2.days.ago) }
+      let(:new_tweet) { create(:tweet, identity: user.identity, twitter_created_at: Time.current) }
+      let!(:new_tweet_metric) { create(:tweet_metric, tweet: new_tweet, pulled_at: Time.current) }
+
+      let(:oldest_metric) { create(:tweet_metric, tweet: old_tweet, pulled_at: 48.hours.ago) }
+      let(:newest_metric) { create(:tweet_metric, tweet: old_tweet, pulled_at: 25.hours.ago) }
+
+      let(:expected_query) do
+        since_time = subject.send(:id_to_time, oldest_metric.tweet.id) - 1
+        until_time = subject.send(:id_to_time, newest_metric.tweet.id) + 1
+        "from:#{old_tweet.identity.handle} since_time:#{since_time} until_time:#{until_time}"
+      end
+
+      it 'updates old tweets and skips new tweets without metrics' do
+        expect(client).to receive(:search_tweets).with(query: expected_query).and_return({ 'data' => [] })
+        expect(client).not_to receive(:search_tweets).with(query: include("from:#{new_tweet.identity.handle}"))
+
+        subject.call
+      end
+    end
+  end
+end
