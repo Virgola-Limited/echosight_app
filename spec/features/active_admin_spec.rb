f5dd49693423df42dba5912f2e88bacac16e6c94
Chris Toynbee <ctoynbee@gmail.com>
2024-04-25 13:47:15 +1200

    Add feature for active admin check

---
+++ spec/features/active_admin_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Active Admin navigation links', type: :feature do
+  let(:admin_user) { create(:admin_user) }
+
+  before do
+    login_as admin_user, scope: :admin_user
+    visit admin_root_path
+  end
+
+  it 'loads all top menu links without errors' do
+    # First, collect all the links
+    links = all('#tabs .menu_item a').map { |link| link[:href] }
+    expect(links.count).to be > 5
+    # Then visit each link
+    links.each do |href|
+      visit href
+      expect(page).not_to have_content('404')
+      expect(page).not_to have_content('500')
+      # Add additional checks as necessary
+    end
+  end
+end
