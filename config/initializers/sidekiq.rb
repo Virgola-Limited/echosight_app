35752e34da841fe94becdb6ee66d5040885d13db
Chris Toynbee <ctoynbee@gmail.com>
2024-05-02 12:04:44 +1200

    Disable api batch cleaner

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -41,11 +41,11 @@ if !Rails.env.development? && !Rails.env.test?
           'cron' => CronExpressionGenerator.for_interval(ApplicationConstants::TWITTER_FETCH_INTERVAL),
           'class' => 'Twitter::TweetsFetcherJob'
         },
-        {
-          'name' => 'Remove old empty ApiBatches',
-          'cron' => '0 0 * * *',
-          'class' => 'RemoveOldEmptyApiBatchJob'
-        }
+        # {
+        #   'name' => 'Remove old empty ApiBatches',
+        #   'cron' => '0 0 * * *',
+        #   'class' => 'RemoveOldEmptyApiBatchJob'
+        # }
       ]
     )
   end
