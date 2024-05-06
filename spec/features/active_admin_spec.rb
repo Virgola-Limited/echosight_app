15a20d99383c0ce6663b18167b97ae0b250828d0
Chris Toynbee <ctoynbee@gmail.com>
2024-05-06 13:15:53 +1200

    Dont check sidekiq link in active admin

--- spec/features/active_admin_spec.rb
+++ spec/features/active_admin_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe 'Active Admin navigation links', type: :feature do
 
   it 'loads all top menu links without errors' do
     # First, collect all the links
-    links = all('#tabs .menu_item a').map { |link| link[:href] }
+    links = all('#tabs .menu_item a').map { |link| [link[:href], link.text] }
     expect(links.count).to be > 5
-    # Then visit each link
-    links.each do |href|
+    # Then visit each link, ignoring the 'sidekiq' link
+    links.each do |href, text|
+      next if text == 'sidekiq'
       visit href
       expect(page).not_to have_content('404')
       expect(page).not_to have_content('500')
