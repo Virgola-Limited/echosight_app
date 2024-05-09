d3452f44f5a099a2bf6739059ab984b09b20a367
Chris Toynbee <ctoynbee@gmail.com>
2024-05-09 13:04:18 +1200

    Remove extra debugging

--- app/services/identity_updater.rb
+++ app/services/identity_updater.rb
@@ -5,7 +5,6 @@ class IdentityUpdater
   attr_reader :user_data
 
   def initialize(user_data)
-    Notifications::SlackNotifier.call(message: "Debugging: Updating identity for #{user_data}.", channel: :errors)
     @user_data = user_data
   end
 
