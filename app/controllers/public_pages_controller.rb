a409030c5f6e980e434f7e32c473aa642f47fa6b
Chris Toynbee <ctoynbee@gmail.com>
2024-05-16 15:54:05 +1200

    Add flash message for not enough data

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -12,6 +12,8 @@ class PublicPagesController < ApplicationController
     if public_page_data.demo? && current_or_guest_user.guest?
       sign_up_link = view_context.link_to('Sign up', new_user_registration_path).html_safe
       flash.now[:notice] = "This is a demo page showing how your public page could look. #{sign_up_link} to get your own!".html_safe
+    elsif !current_user.enough_data_for_public_page?
+      flash.now[:notice] = "We haven't collected enough data to make your public page look great yet. Please check back later.".html_safe
     end
 
     render PublicPageComponent.new(public_page_data: public_page_data, current_user: current_or_guest_user)
