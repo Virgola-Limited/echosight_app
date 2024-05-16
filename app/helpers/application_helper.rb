aca6b227bc9cefbbf7e642523e9352689ea50fc3
Dean Lofts <dean@deanlofts.xyz>
2024-05-16 21:03:10 +1000

    leaderboard things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
   include ActionView::Helpers::UrlHelper
 
+  def number_to_human_readable(number)
+    if number >= 1_000_000
+      "#{(number / 1_000_000).floor}M"
+    elsif number >= 1_000
+      "#{(number / 1_000).floor}K"
+    else
+      number.to_s
+    end
+  end
+
   def html_description_with_links(description)
-    description.to_s.gsub(/(https?:\/\/[^\s]+)/) do |url|
+    description.to_s.gsub(%r{(https?://[^\s]+)}) do |url|
       uri = URI.parse(url)
       host_and_path = uri.host + uri.path.chomp('/')
 
