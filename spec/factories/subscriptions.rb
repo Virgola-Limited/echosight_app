8e5b36ce97c9d2663b4c6a933982c05c34b1bea4
Chris Toynbee <ctoynbee@gmail.com>
2024-05-20 11:02:39 +1200

    Add tests for definition of syncable

---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id                     :bigint           not null, primary key
+#  active                 :boolean          default(TRUE)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  stripe_price_id        :string
+#  stripe_subscription_id :string
+#  user_id                :bigint           not null
+#
+# Indexes
+#
+#  index_subscriptions_on_stripe_price_id         (stripe_price_id)
+#  index_subscriptions_on_stripe_subscription_id  (stripe_subscription_id)
+#  index_subscriptions_on_user_id                 (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :subscription do
+    # Define your factory attributes here
+    active { true }
+    stripe_subscription_id { Faker::Alphanumeric.alphanumeric(number: 10) }
+    stripe_price_id { Faker::Alphanumeric.alphanumeric(number: 10) }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,104 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  invitation_accepted_at :datetime
+#  invitation_created_at  :datetime
+#  invitation_limit       :integer
+#  invitation_sent_at     :datetime
+#  invitation_token       :string
+#  invitations_count      :integer          default(0)
+#  invited_by_type        :string
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  invited_by_id          :bigint
+#  stripe_customer_id     :string
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_invitation_token      (invitation_token) UNIQUE
+#  index_users_on_invited_by            (invited_by_type,invited_by_id)
+#  index_users_on_invited_by_id         (invited_by_id)
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_stripe_customer_id    (stripe_customer_id)
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User do
+  subject { described_class.new }
+
+
+  describe '#syncable?' do
+    context 'when the user is not confirmed' do
+      let(:user) { create(:user, confirmed_at: nil) }
+
+      it 'returns false' do
+        expect(user.syncable?).to be_falsey
+      end
+    end
+
+    context 'when the user is confirmed' do
+      context 'when the user has no identity' do
+        let(:user) { create(:user, confirmed_at: Time.current) }
+
+        it 'returns false' do
+          expect(user.syncable?).to be_falsey
+        end
+      end
+
+      context 'when the user has an identity' do
+        context 'when the identity is not valid' do
+          let!(:identity) { create(:identity, user: user, provider: 'facepalm')}
+          let(:user) { create(:user, confirmed_at: Time.current) }
+
+          it 'returns false' do
+            expect(user.syncable?).to be_falsey
+          end
+        end
+
+        context 'when the identity is valid' do
+          context 'when the user does not have an active subscription' do
+            let(:user) { create(:user, confirmed_at: Time.current) }
+
+            it 'returns false' do
+              expect(user.syncable?).to be_falsey
+            end
+          end
+
+          context 'when the user has an active subscription' do
+            let!(:identity) { create(:identity, user: user)}
+            let(:user) { create(:user, confirmed_at: Time.current) }
+            let!(:subscription) { create(:subscription, user: user, active: true) }
+
+            it 'returns true' do
+              expect(user.syncable?).to be_truthy
+            end
+          end
+        end
+      end
+    end
+  end
+end
