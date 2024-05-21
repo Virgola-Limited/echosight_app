84256335bf9df01c8d61217ded26977ce8b1c716
Chris Toynbee <ctoynbee@gmail.com>
2024-05-21 13:56:31 +1200

    Improve UI of dashboard

--- app/components/content_box_component.rb
+++ app/components/content_box_component.rb
@@ -3,7 +3,7 @@
 class ContentBoxComponent < ApplicationComponent
 
   def call
-    content_tag :div, class: 'w-full max-w-xl space-y-4 p-4 bg-white rounded-lg shadow dark:bg-gray-800 mb-4' do
+    content_tag :div, class: 'w-full space-y-4 p-4 bg-white rounded-lg shadow dark:bg-gray-800 mb-4' do
       content
     end
   end
