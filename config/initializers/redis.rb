2b6cb113fe9525720b59e12256fa3c68a8bdf1c7
Chris Toynbee <ctoynbee@gmail.com>
2024-05-28 10:13:11 +1200

    Attempt to fix A RedisClient::CannotConnectError

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+# config/initializers/redis.rb
+
+$redis = Redis.new(
+  url: ENV["REDIS_URL"],
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+  },
+  connect_timeout: 2,   # Default is 1 second
+  read_timeout: 2,      # Default is 1 second
+  write_timeout: 2      # Default is 1 second
+)
