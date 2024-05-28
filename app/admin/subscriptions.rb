19a1b7e413b0bb17a053a809137f3b750c0ad9ee
Chris Toynbee <ctoynbee@gmail.com>
2024-05-29 08:51:48 +1200

    Add public page to subscriptions

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -1,2 +1,17 @@
 ActiveAdmin.register Subscription do
+  index do
+    selectable_column
+    id_column
+    column :active
+    column :created_at
+    column :updated_at
+    column :stripe_price_id
+    column :stripe_subscription_id
+    column :user_id
+    actions defaults: true do |subscription|
+      link_to "Public Page", public_page_path(handle: subscription.user.identity.handle), target: "_blank"
+    end
+  end
+  # Additional configurations or customizations if needed
+
 end
