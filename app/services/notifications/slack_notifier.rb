a57316ed746618d1fd9d7d69794a0464a6fabbc7
Chris <570876+tophertoy@users.noreply.github.com>
2024-05-28 18:00:57 +1200

    Ct/better slack stripe updates (#91)
    * Better slack updates on subscription info
    * Send staging messages to a separate slack channel
    * Tests for event handler

--- app/services/notifications/slack_notifier.rb
+++ app/services/notifications/slack_notifier.rb
@@ -4,7 +4,7 @@ module Notifications
       return if ENV['DISABLE_SLACK_NOTIFY']
 
       webhook_urls = Rails.application.credentials.slack[:webhook_url]
-      webhook_url = webhook_urls[channel] || webhook_urls[:general]
+      webhook_url = webhook_urls[Rails.env.to_sym] || webhook_urls[channel] || webhook_urls[:general]
 
       notifier = Slack::Notifier.new(webhook_url)
       notifier.ping("#{Rails.env.upcase}: #{message}")
