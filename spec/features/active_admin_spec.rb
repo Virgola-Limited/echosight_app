74ec205c18b7e0b28fae097189fc39f4e54de834
Chris Toynbee <ctoynbee@gmail.com>
2024-05-30 10:05:14 +1200

    Add specs for active admin

--- spec/features/active_admin_spec.rb
+++ spec/features/active_admin_spec.rb
@@ -8,16 +8,26 @@ RSpec.describe 'Active Admin navigation links' do
     visit admin_root_path
   end
 
+  def links
+    all('#tabs .menu_item a').map { |link| [link[:href], link.text.strip] }.reject { |href, text| text == 'PgHero' || text == 'Sidekiq'}
+  end
+
   it 'loads all top menu links without errors' do
-    # First, collect all the links
-    links = all('#tabs .menu_item a').map { |link| [link[:href], link.text] }
     expect(links.count).to be > 5
-    # Then visit each link, ignoring the 'sidekiq' link
+
     links.each do |href, text|
-      next if text == 'Sidekiq'
       visit href
       expect(page).not_to have_content('404')
       expect(page).not_to have_content('500')
+    end
+  end
+
+  it 'redirects to login page if not logged in' do
+    logout(:admin_user)
+
+    links.each do |href, _text|
+      visit href
+      expect(current_path).to eq(new_admin_user_session_path)
     end
   end
 end
