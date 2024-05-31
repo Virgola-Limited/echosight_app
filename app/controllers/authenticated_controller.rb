d7e02c397811969228257d3f320f4c5215608674
Chris Toynbee <ctoynbee@gmail.com>
2024-06-01 10:31:02 +1200

    Add flash notice

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -4,6 +4,7 @@ class AuthenticatedController < ApplicationController
 
   def authenticate_user!
     unless user_signed_in?
+      flash[:notice] = 'You must be signed in to access this page.'
       redirect_to new_user_registration_url
     end
   end
