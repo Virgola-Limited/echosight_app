fdc4c5b245165d02559b86da34a23e106319ca9f
Chris Toynbee <ctoynbee@gmail.com>
2024-06-01 10:11:38 +1200

    Subscription tests

---
+++ spec/features/subscription_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Subscription' do
+  scenario 'Subscription page in various states' do
+    # Context: When the user is not logged in
+    # visit subscription_path
+    # expect(page).to have_text('You need to sign in or sign up before continuing.')
+
+
+    # Context: when the user has just signed up
+  end
+end
