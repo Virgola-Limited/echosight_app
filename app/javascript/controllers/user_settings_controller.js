e06a462df65a264f6491e47e1077db37ff3949a2
Chris <570876+tophertoy@users.noreply.github.com>
2024-06-02 12:03:29 +1200

    105 Add debounce to subscription button to prevent multiple subscriptions (#106)
    * Make button component work with a form or without
    * add debounce to subscription button

--- app/javascript/controllers/user_settings_controller.js
+++ app/javascript/controllers/user_settings_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = [ "toggle" ]
-
-  connect() {
-    console.log('Stimulus controller connected')
-  }
-
   updateSetting(event) {
     const key = event.target.name.split("[")[1].split("]")[0]
     const value = event.target.checked ? 'true' : 'false'
