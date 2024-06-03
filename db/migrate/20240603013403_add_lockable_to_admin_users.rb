471e48aaede805251139dca6a972c4e87017c7f7
Chris Toynbee <ctoynbee@gmail.com>
2024-06-03 13:41:02 +1200

    Active admin lockable

---
+++ db/migrate/20240603013403_add_lockable_to_admin_users.rb
@@ -0,0 +1,4 @@
+class AddLockableToAdminUsers < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_02_230229) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_013403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
