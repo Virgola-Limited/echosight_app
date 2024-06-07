675085dfe0348f8f759272cee8ca6c71ba6867d9
Chris Toynbee <ctoynbee@gmail.com>
2024-06-08 10:36:06 +1200

    Sharable top posts

--- app/javascript/entrypoints/share_tool.js
+++ app/javascript/entrypoints/share_tool.js
@@ -14,6 +14,37 @@ function removeExtraPadding() {
   });
 }
 
+function addText(){
+  return
+  const context = canvas.getContext('2d');
+  const text = "https://app.echosight.io";
+  const fontSize = 20;  // Increased font size
+  const padding = 10;
+
+  context.font = `${fontSize}px Arial`;
+  context.fillStyle = 'black';
+  context.textAlign = 'right';
+  context.textBaseline = 'bottom';
+
+  const x = canvas.width - padding;
+  const y = canvas.height - padding;
+
+  const textWidth = context.measureText(text).width;
+  const textHeight = fontSize;
+  context.fillStyle = 'rgba(255, 255, 255, 0.7)';
+  context.fillRect(x - textWidth - 5, y - textHeight - 5, textWidth + 10, textHeight + 10);
+
+  context.fillStyle = 'black';
+  context.fillText(text, x, y);
+}
+
+function debugSharing(){
+  console.log('Modal ID:', modalId);
+  console.log('Chart ID:', chartId);
+  console.log('Modal Content:', modalContent);
+  console.log('Chart Element:', chartElement);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('[data-modal-toggle]').forEach(modalToggleButton => {
     modalToggleButton.addEventListener('click', function () {
@@ -21,13 +52,13 @@ document.addEventListener('DOMContentLoaded', function () {
       const chartId = modalToggleButton.getAttribute('data-chart-id');
       const modalContent = document.querySelector(`#${modalId} .p-4.md\\:p-5.space-y-4`);
       const chartElement = document.getElementById(chartId);
-
-      // console.log('Modal ID:', modalId);
-      // console.log('Chart ID:', chartId);
-      // console.log('Modal Content:', modalContent);
-      // console.log('Chart Element:', chartElement);
-
+      console.log(chartElement);
       if (chartElement) {
+        const originalDisplay = chartElement.style.display;
+        console.log('originalDisplay', originalDisplay);
+        if(originalDisplay !== 'block'){
+          chartElement.style.display = 'block';
+        }
         const elementsToHide = document.querySelectorAll('.hide-from-share');
         elementsToHide.forEach(element => {
           element.style.visibility = 'hidden';
@@ -35,26 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
         addExtraPadding();
 
         html2canvas(chartElement).then(canvas => {
-          // const context = canvas.getContext('2d');
-          // const text = "https://app.echosight.io";
-          // const fontSize = 20;  // Increased font size
-          // const padding = 10;
-
-          // context.font = `${fontSize}px Arial`;
-          // context.fillStyle = 'black';
-          // context.textAlign = 'right';
-          // context.textBaseline = 'bottom';
-
-          // const x = canvas.width - padding;
-          // const y = canvas.height - padding;
-
-          // const textWidth = context.measureText(text).width;
-          // const textHeight = fontSize;
-          // context.fillStyle = 'rgba(255, 255, 255, 0.7)';
-          // context.fillRect(x - textWidth - 5, y - textHeight - 5, textWidth + 10, textHeight + 10);
-
-          // context.fillStyle = 'black';
-          // context.fillText(text, x, y);
+          addText()
 
           modalContent.innerHTML = '';
 
@@ -68,6 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
             element.style.visibility = 'visible';
           });
           removeExtraPadding();
+          chartElement.style.display = originalDisplay;
 
           // Add copy functionality
           const copyButton = document.querySelector(`#${modalId} .copy-button`);
