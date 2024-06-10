cc0ae073c54621f55b852df04193856a460eaf81
Chris Toynbee <ctoynbee@gmail.com>
2024-06-10 18:50:46 +1200

    Remove name from title

--- app/components/shared/heading_component.rb
+++ app/components/shared/heading_component.rb
@@ -1,8 +1,8 @@
 module Shared
-  class HeadingComponent <  ApplicationComponent
-    attr_reader :heading_type
+  class HeadingComponent < ApplicationComponent
+    attr_reader :heading_type, :text
 
-    def initialize(text:, heading_type:)
+    def initialize(text: nil, heading_type:)
       @text = text
       @heading_type = heading_type
     end
@@ -12,7 +12,7 @@ module Shared
       when :h1
         "text-5xl font-extrabold dark:text-white"
       when :h2
-        "text-2xl font-bold text-gray-900 dark:text-white"
+        "text-2xl sm:text-lg font-bold text-gray-900 dark:text-white"
       when :h3
         "text-1xl font-bold dark:text-white"
       when :h4
