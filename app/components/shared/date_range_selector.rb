7d727774affa2944c152e0c6f73c5b9496dade21
Chris Toynbee <ctoynbee@gmail.com>
2024-06-17 09:37:12 +1200

    Style date selector

--- app/components/shared/date_range_selector.rb
+++ app/components/shared/date_range_selector.rb
@@ -1,9 +1,14 @@
 module Shared
   class DateRangeSelector < ApplicationComponent
-    attr_reader :page_user_handle
+    attr_reader :page_user_handle, :date_range
 
-    def initialize(page_user_handle:)
+    def initialize(page_user_handle:, date_range: '7d')
       @page_user_handle = page_user_handle
+      @date_range = date_range
+    end
+
+    def selected_class(range)
+      'bg-blue-600 text-white dark:bg-blue-600 dark:text-white' if date_range == range
     end
   end
 end
