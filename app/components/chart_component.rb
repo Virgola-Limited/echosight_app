b349a5f1acb936cd6eb00eb1faf6ba670a76a5b1
Chris Toynbee <ctoynbee@gmail.com>
2024-06-20 17:51:28 +1200

    Fix formatting

--- app/components/chart_component.rb
+++ app/components/chart_component.rb
@@ -1,12 +1,33 @@
 # app/components/chart_component.rb
 class ChartComponent < ViewComponent::Base
-  def initialize(chart_id:, chart_type:, series_name:, series_data:, categories:, y_formatter:, y_suffix: '')
+  def initialize(chart_id:, chart_type:, series_name:, series_data:, categories:, y_suffix: '')
     @chart_id = chart_id
     @chart_type = chart_type
     @series_name = series_name
     @series_data = series_data
     @categories = categories
-    @y_formatter = y_formatter
     @y_suffix = y_suffix
+  end
+
+  def y_formatter
+    <<-JS
+      function (val) {
+        if (val >= 1000000) {
+          let formattedVal = (val / 1000000).toFixed(2);
+          if (formattedVal.endsWith('00')) {
+            formattedVal = (val / 1000000).toFixed(0);
+          }
+          return formattedVal + 'M#{@y_suffix}';
+        } else if (val >= 1000) {
+          let formattedVal = (val / 1000).toFixed(2);
+          if (formattedVal.endsWith('00')) {
+            formattedVal = (val / 1000).toFixed(0);
+          }
+          return formattedVal + 'K#{@y_suffix}';
+        } else {
+          return val.toFixed(2) + '#{@y_suffix}';
+        }
+      }
+    JS
   end
 end
