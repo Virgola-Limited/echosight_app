d3198857c47c29587417a60b518b78e5bf97072f
Chris Toynbee <ctoynbee@gmail.com>
2024-06-22 08:27:25 +1200

    Configure vite for test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,32 @@ import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
 
-export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-    FullReload(['config/routes.rb', 'app/views/**/*', 'app/javascript/controllers/**/*'], { delay: 200 }),
-    StimulusHMR(),
-  ],
-  server: {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST', 'OPTIONS', 'HEAD'],
-      headers: ['Authorization', 'Content-Type'],
-      credentials: true,
-      maxAge: 600,
-    },
-    host: 'localhost',
-    port: 3000,
-    hmr: {
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      RubyPlugin(),
+      FullReload(['config/routes.rb', 'app/views/**/*', 'app/javascript/controllers/**/*'], { delay: 200 }),
+      StimulusHMR(),
+    ],
+    server: {
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'OPTIONS', 'HEAD'],
+        headers: ['Authorization', 'Content-Type'],
+        credentials: true,
+        maxAge: 600,
+      },
       host: 'localhost',
+      port: 3000,
+      hmr: {
+        host: 'localhost',
+      },
     },
-  },
+    build: {
+      outDir: mode === 'test' ? 'public/assets-test' : 'public/assets',
+    },
+    test: {
+      environment: 'jsdom',
+    },
+  }
 })
