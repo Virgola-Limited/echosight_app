550096a0cc9366a899a0703c898534a6d84a4c9e
Chris Toynbee <ctoynbee@gmail.com>
2024-06-27 11:54:21 +1200

    Fix bug in cache regeneration

--- app/jobs/regenerate_user_public_page_cache_job.rb
+++ app/jobs/regenerate_user_public_page_cache_job.rb
@@ -1,10 +1,9 @@
-# app/jobs/regenerate_user_public_page_cache_job.rb
 class RegenerateUserPublicPageCacheJob
   include Sidekiq::Job
   include Cacheable
 
   def perform
-    Identity.syncable.find_each do |identity|
+    Identity.where(sync_without_user: true).or(Identity.where.not(user_id: nil)).find_each do |identity|
       regenerate_cache_for_identity(identity)
     end
   end
@@ -13,8 +12,7 @@ class RegenerateUserPublicPageCacheJob
 
   def regenerate_cache_for_identity(identity)
     service = PublicPageService.new(handle: identity.handle, current_user: identity.user, date_range: Date.current)
-    cache_key = service.cache_key_for_user_public_page(service.user, date_range: Date.current, hours: 24)
-    Rails.cache.fetch(cache_key, expires_in: 24.hours) do
+    Rails.cache.fetch(service.cache_key, expires_in: 24.hours) do
       service.generate_public_page_data
     end
   end
