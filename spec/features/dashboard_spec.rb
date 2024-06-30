25b546b955ba8aab998c8b1f84c591a2d801181b
Chris Toynbee <ctoynbee@gmail.com>
2024-06-30 18:34:20 +1200

    Improve test

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -11,6 +11,10 @@ RSpec.feature 'Dashboard' do
     login_as(user, scope: :user)
     visit dashboard_index_path
     expect(page).to have_text('To start leveraging the full capabilities')
+    expect(page).to have_text('Connect to Twitter/X')
+    expect(page).to have_text('Subscribe')
+
+    click_button 'Connect to Twitter/X'
 
   end
 
