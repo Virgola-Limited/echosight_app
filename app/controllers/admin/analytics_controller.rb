6ff9cd827fc11ff3ec559c5d95248d47037ba2fe
Chris Toynbee <ctoynbee@gmail.com>
2024-07-08 13:58:27 +1200

    Add menu to active admin

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -5,12 +5,11 @@ module Admin
 
     def dashboard
       @campaign_sign_ups = User.where.not(campaign_id: nil).group("DATE(created_at)", :campaign_id).count
-      Rails.logger.debug('paul' + @campaign_sign_ups.inspect)
-      # Prepare data for the chart component
+
       @categories = @campaign_sign_ups.keys.map { |(date, _)| date }.uniq.sort
       @series_data = @campaign_sign_ups.group_by { |(_, campaign)| campaign }.map do |campaign, data|
         {
-          name: campaign,
+          name: campaign.to_s || "Unknown Campaign", # Ensure name is a string
           data: @categories.map { |date| data.find { |(d, _), _| d == date }&.last || 0 }
         }
       end
