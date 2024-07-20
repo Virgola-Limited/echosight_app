5ff8019eb678e9ee56541a33cafb10b4756f611a
Chris Toynbee <ctoynbee@gmail.com>
2024-07-20 21:38:05 +1200

    Fix incorrect file name and module

--- app/jobs/subscription_sync_job.rb
+++ app/jobs/subscription_sync_job.rb
@@ -1,6 +1,8 @@
 # app/jobs/subscription_sync_job.rb
-class SubscriptionSyncJob < ApplicationJob
-  queue_as :default
+class SubscriptionSyncJob
+  include Sidekiq::Job
+
+  sidekiq_options queue: :low_priority
 
   def perform
     Subscription.where(active: true).find_each do |subscription|
