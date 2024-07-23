e85294a0e933499a61015cdb7774f426dd84a556
Chris Toynbee <ctoynbee@gmail.com>
2024-07-23 17:57:24 +1200

    tidy up

--- app/queries/twitter/leaderboard_query.rb
+++ app/queries/twitter/leaderboard_query.rb
@@ -9,8 +9,7 @@ module Twitter
     end
 
     def identity_leaderboard
-      metrics_query = Twitter::TweetMetrics::ImpressionsQuery.new(identity: nil, date_range: date_range)
-      metrics_data = metrics_query.aggregated_metrics_for_all_identities
+      metrics_data = aggregated_metrics_for_all_identities
 
       leaderboard_data = metrics_data.map do |data|
         identity = Identity.find(data.identity_id)
@@ -44,6 +43,27 @@ module Twitter
 
     def parsed_date_range
       @parsed_date_range ||= Twitter::DateRangeOptions.parse_date_range(date_range)
+    end
+
+    def aggregated_metrics_for_all_identities
+      start_time = parsed_date_range[:start_time]
+      end_time = parsed_date_range[:end_time]
+
+      # Subquery to get the first tweet_metric for each tweet
+      subquery = TweetMetric.select('DISTINCT ON (tweet_metrics.tweet_id) tweet_metrics.tweet_id, tweet_metrics.impression_count AS first_impression_count, tweet_metrics.retweet_count AS first_retweet_count, tweet_metrics.like_count AS first_like_count, tweet_metrics.quote_count AS first_quote_count, tweet_metrics.reply_count AS first_reply_count, tweet_metrics.bookmark_count AS first_bookmark_count')
+                            .where('tweet_metrics.created_at BETWEEN ? AND ?', start_time, end_time)
+                            .order('tweet_metrics.tweet_id, tweet_metrics.created_at ASC')
+
+      Tweet.joins("INNER JOIN (#{subquery.to_sql}) AS first_metrics ON tweets.id = first_metrics.tweet_id")
+           .where(twitter_created_at: start_time..end_time)
+           .group('tweets.identity_id')
+           .select('tweets.identity_id,
+                    SUM(first_metrics.first_impression_count) AS total_impressions,
+                    SUM(first_metrics.first_retweet_count) AS total_retweets,
+                    SUM(first_metrics.first_like_count) AS total_likes,
+                    SUM(first_metrics.first_quote_count) AS total_quotes,
+                    SUM(first_metrics.first_reply_count) AS total_replies,
+                    SUM(first_metrics.first_bookmark_count) AS total_bookmarks')
     end
   end
 end
--- app/queries/twitter/tweet_metrics/impressions_query.rb
+++ app/queries/twitter/tweet_metrics/impressions_query.rb
@@ -34,27 +34,6 @@ module Twitter
           { date: date, data_points: impressions_sum, formatted_label: formatted_label }
         end
       end
-
-      def aggregated_metrics_for_all_identities
-        start_time = date_range[:start_time]
-        end_time = date_range[:end_time]
-
-        # Subquery to get the first tweet_metric for each tweet
-        subquery = TweetMetric.select('DISTINCT ON (tweet_metrics.tweet_id) tweet_metrics.tweet_id, tweet_metrics.impression_count AS first_impression_count, tweet_metrics.retweet_count AS first_retweet_count, tweet_metrics.like_count AS first_like_count, tweet_metrics.quote_count AS first_quote_count, tweet_metrics.reply_count AS first_reply_count, tweet_metrics.bookmark_count AS first_bookmark_count')
-                              .where('tweet_metrics.created_at BETWEEN ? AND ?', start_time, end_time)
-                              .order('tweet_metrics.tweet_id, tweet_metrics.created_at ASC')
-
-        Tweet.joins("INNER JOIN (#{subquery.to_sql}) AS first_metrics ON tweets.id = first_metrics.tweet_id")
-             .where(twitter_created_at: start_time..end_time)
-             .group('tweets.identity_id')
-             .select('tweets.identity_id,
-                      SUM(first_metrics.first_impression_count) AS total_impressions,
-                      SUM(first_metrics.first_retweet_count) AS total_retweets,
-                      SUM(first_metrics.first_like_count) AS total_likes,
-                      SUM(first_metrics.first_quote_count) AS total_quotes,
-                      SUM(first_metrics.first_reply_count) AS total_replies,
-                      SUM(first_metrics.first_bookmark_count) AS total_bookmarks')
-      end
     end
   end
 end
