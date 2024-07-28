d32fd35327672d4bda9ca6afb9d743d7bfbd9080
Chris Toynbee <ctoynbee@gmail.com>
2024-07-29 10:42:49 +1200

    Show different primary actions depending on the page

--- app/components/primary_action_button_component.rb
+++ app/components/primary_action_button_component.rb
@@ -1,45 +1,64 @@
 # frozen_string_literal: true
 
 class PrimaryActionButtonComponent < ApplicationComponent
-  attr_reader :user, :url, :link_text, :button_classes
+  attr_reader :user, :url, :link_text, :request
 
-  def initialize(user:, url: nil)
+  def initialize(user:, request:, url: nil)
     @user = user
+    @request = request
     @url = url
-    @link_text = button_text.sample
+    @link_text = determine_button_text
   end
 
   def before_render
-    @url ||= button_url
+    @url ||= determine_button_url
   end
 
   def call
-    link_to @url do
-      render Shared::ButtonComponent.new(
-        text: @link_text,
-        url: @url,
-        classes: responsive_classes,
-        no_form: true
-      )
-    end
+    render Shared::ButtonComponent.new(
+      text: @link_text,
+      url: @url,
+      classes: 'w-full sm:flex md:block',
+      no_form: true,
+      size: :extra_small,
+      method: :get
+    )
   end
 
   private
 
+  def determine_button_text
+    if show_login_button?
+      'Login'
+    elsif user.guest?
+      button_text.sample
+    else
+      'Dashboard'
+    end
+  end
+
+  def determine_button_url
+    if show_login_button?
+      new_user_session_path
+    elsif user.guest?
+      new_user_registration_path
+    else
+      dashboard_index_path
+    end
+  end
+
+  def show_login_button?
+    [
+      '/users/sign_up',
+      '/users/password/new',
+      '/users/confirmation/new'
+    ].include?(request.path)
+  end
+
   def button_text
-    return ['Dashboard'] unless user.guest?
     [
       'Get Your Personal Stats Dashboard',
       'Start Tracking Your Twitter/X Impact'
     ]
-  end
-
-  def button_url
-    return dashboard_index_path unless user.guest?
-    new_user_registration_path
-  end
-
-  def responsive_classes
-    "md:px-3 md:py-2 md:text-sm lg:px-5 lg:py-3 lg:text-base"
   end
 end
