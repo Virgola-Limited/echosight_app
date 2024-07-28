e48cd737cc0e0d10515e3ca7a7d8bac6681edf6f
Chris Toynbee <ctoynbee@gmail.com>
2024-07-28 19:49:00 +1200

    My posts component

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,8 @@
 class PostsController < AuthenticatedController
-  include Pagy::Backend
-
   def index
-    if params[:query].present?
-      query = params[:query].split.map { |word| "#{word}:*" }.join(' & ')
-      tsquery = ActiveRecord::Base.sanitize_sql_array(["to_tsquery('english', ?)", query])
-
-      results = current_user.tweets.where("searchable @@ #{tsquery}")
-                     .order(Arel.sql("ts_rank(searchable, #{tsquery}) DESC"))
-
-    else
-      results = current_user.tweets
+    unless current_user.active_subscription?
+      flash.now[:alert] = "To see your posts you need a valid subscription. For now, we've shown you some example posts to see how it would work."
     end
-
-    @pagy, @posts = pagy(results, items: 5)
+    render Posts::IndexComponent.new(current_user: current_user, query: params[:query])
   end
 end
