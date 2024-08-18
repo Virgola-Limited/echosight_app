d2dd5358aa464890904f74241ce1d7bebcc4abbc
Chris Toynbee <ctoynbee@gmail.com>
2024-08-19 08:50:19 +1200

    debug jemalloc

--- .render/build.sh
+++ .render/build.sh
@@ -5,7 +5,6 @@ set -x  # Enable debugging
 echo "==== Build Environment ===="
 echo "PWD: $PWD"
 echo "HOME: $HOME"
-echo "RENDER_PROJECT_DIR: $RENDER_PROJECT_DIR"
 
 # Install jemalloc in the project directory
 JEMALLOC_VERSION=5.3.0
@@ -24,12 +23,14 @@ echo "Verifying installation:"
 ls -l $JEMALLOC_DIR/lib
 file $JEMALLOC_DIR/lib/libjemalloc.so
 
-echo "Setting up LD_PRELOAD and LD_LIBRARY_PATH"
+# Create a script to set environment variables
+cat << EOF > $PWD/set_jemalloc_env.sh
 export LD_PRELOAD="$JEMALLOC_DIR/lib/libjemalloc.so"
 export LD_LIBRARY_PATH="$JEMALLOC_DIR/lib:$LD_LIBRARY_PATH"
+EOF
 
-echo "LD_PRELOAD is now set to: $LD_PRELOAD"
-echo "LD_LIBRARY_PATH is now set to: $LD_LIBRARY_PATH"
+echo "Created set_jemalloc_env.sh script:"
+cat $PWD/set_jemalloc_env.sh
 
 # Run the rest of your build process
 cd $PWD
@@ -40,12 +41,3 @@ RAILS_ENV=production bin/rails assets:precompile
 
 echo "==== Final Build Directory Structure ===="
 ls -R $PWD/jemalloc
-
-# Create a script to set environment variables
-cat << EOF > set_jemalloc_env.sh
-export LD_PRELOAD="$JEMALLOC_DIR/lib/libjemalloc.so"
-export LD_LIBRARY_PATH="$JEMALLOC_DIR/lib:$LD_LIBRARY_PATH"
-EOF
-
-echo "Created set_jemalloc_env.sh script"
-cat set_jemalloc_env.sh
