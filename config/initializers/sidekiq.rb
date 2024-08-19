3b0b5acd006a1aa512a05cd4efa286f327985202
Chris Toynbee <ctoynbee@gmail.com>
2024-08-20 07:09:23 +1200

    optionally disable sidekiq jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -22,46 +22,48 @@ Sidekiq.configure_server do |config|
   end
 
   Sidekiq::Cron::Job.destroy_all!
-  Sidekiq::Cron::Job.load_from_array!(
-    [
-      {
-        'name' => 'Queue Monitor - every 5 minutes',
-        'cron' => '*/5 * * * *',
-        'class' => 'QueueMonitorJob'
-      },
-      {
-        'name' => 'Fetch Tweets',
-        'cron' => CronExpressionGenerator.for_interval(ApplicationConstants::TWITTER_FETCH_INTERVAL),
-        'class' => 'Twitter::TweetsFetcherJob'
-      },
-      {
-        'name' => 'Sync Subscriptions - every 1 hour',
-        'cron' => '0 * * * *',
-        'class' => 'SubscriptionSyncJob'
-      },
-      {
-        'name' => 'Regenerate User Public Page Cache - every 3 hours',
-        'cron' => '0 */3 * * *',
-        'class' => 'RegenerateUserPublicPageCacheJob'
-      },
-      {
-        'name' => 'Users Without Subscription Email - every 1 day',
-        'cron' => '0 0 * * *',
-        'class' => 'UsersWithoutSubscriptionEmailJob'
-      },
-      {
-        'name' => 'capture_leaderboard_job',
-        'cron' => '0 * * * *',
-        'class' => 'Twitter::CaptureLeaderboardJob'
-      },
-      {
-        'name' => 'identity_notifications_job',
-        'cron' => "0 10 * * *",
-        'class' => 'IdentityNotificationJob',
-        'tz' => "Australia/Sydney"
-      }
-    ]
-  )
+  unless ENV['DISABLE_SIDEKIQ_CRON']
+    Sidekiq::Cron::Job.load_from_array!(
+      [
+        {
+          'name' => 'Queue Monitor - every 5 minutes',
+          'cron' => '*/5 * * * *',
+          'class' => 'QueueMonitorJob'
+        },
+        {
+          'name' => 'Fetch Tweets',
+          'cron' => CronExpressionGenerator.for_interval(ApplicationConstants::TWITTER_FETCH_INTERVAL),
+          'class' => 'Twitter::TweetsFetcherJob'
+        },
+        {
+          'name' => 'Sync Subscriptions - every 1 hour',
+          'cron' => '0 * * * *',
+          'class' => 'SubscriptionSyncJob'
+        },
+        {
+          'name' => 'Regenerate User Public Page Cache - every 3 hours',
+          'cron' => '0 */3 * * *',
+          'class' => 'RegenerateUserPublicPageCacheJob'
+        },
+        {
+          'name' => 'Users Without Subscription Email - every 1 day',
+          'cron' => '0 0 * * *',
+          'class' => 'UsersWithoutSubscriptionEmailJob'
+        },
+        {
+          'name' => 'capture_leaderboard_job',
+          'cron' => '0 * * * *',
+          'class' => 'Twitter::CaptureLeaderboardJob'
+        },
+        {
+          'name' => 'identity_notifications_job',
+          'cron' => "0 10 * * *",
+          'class' => 'IdentityNotificationJob',
+          'tz' => "Australia/Sydney"
+        }
+      ]
+    )
+  end
 end
 
 Sidekiq.configure_client do |config|
